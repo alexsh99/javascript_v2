13ca1f56fbcb718db0abdfd8ab97c995f2429c0d
Александр <ash@isnet.ru>
2021-06-15 20:30:31 +0500

    Lesson 2

--- js/main.js
+++ js/main.js
@@ -1,26 +1,76 @@
-const products = [
-    {id: 1, title: "Notebook", price: 2000, icon: 'notebook.jpg', desk: 'Быстрый ноутбук'},
-    {id: 2, title: 'Mouse', price: 20, icon: 'mouse.jpg', desk: 'Отличная мышка'},
-    {id: 3, title: 'Keyboard', price: 200, icon: 'keyboard.jpg'},
-    {id: 4, title: 'Gamepad', price: 50, icon: 'gamepad.jpg', desk: 'Удобный геймпад'},
-    {}
+class BasketList {
+    constructor() {
+        this.products = [];
+    }
+    addProduct(){
 
-];
-//Функция для формирования верстки каждого товара
-//Добавить в выводе изображение
-const renderProduct = (product) => {
-    return `<div class="product-item">
-                <img src="img/${product.icon ?? "no-image.jpg"}" alt="">
-                <h3>${product.title ?? "Название продукта"}</h3>
-                <p class="desk">${product.desk ?? 'Краткое описание'}</p>
-                <p class="price">${product.price ?? "Халява сэр!!!"}</p>
+    }
+    delProduct(){
+
+    }
+    getSum(){
+
+    }
+    getCount(){
+
+    }
+}
+
+class BasketItem {
+    render(){
+
+    }
+}
+
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();
+    }
+
+    _fetchProducts(){
+        this.goods = [
+            {id: 1, title: "Notebook", price: 2000, img: 'notebook.jpg', desk: 'Быстрый ноутбук'},
+            {id: 2, title: 'Mouse', price: 20, img: 'mouse.jpg', desk: 'Отличная мышка'},
+            {id: 3, title: 'Keyboard', price: 200, img: 'keyboard.jpg'},
+            {id: 4, title: 'Gamepad', price: 50, img: 'gamepad.jpg', desk: 'Удобный геймпад'},
+        ];
+    }
+
+    getFullPrice(){
+        return this.goods.reduce((a, p) => a + p.price, 0);
+    }
+
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods){
+            const productObj = new ProductItem(product);
+            block.insertAdjacentHTML('beforeend', productObj.render());
+        }
+    }
+}
+
+
+class ProductItem {
+    constructor(product) {
+        this.id = product.id;
+        this.title = product.title;
+        this.price = product.price;
+        this.img = product.img;
+        this.desk = product.desk;
+    }
+
+    render(){
+        return `<div class="product-item" data-id="${this.id}">
+                <img src="img/${this.img ?? "no-image.jpg"}" alt="">
+                <h3>${this.title ?? "Название продукта"}</h3>
+                <p class="desk">${this.desk ?? 'Краткое описание'}</p>
+                <p class="price">${this.price ?? "Халява сэр!!!"}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
-};
-const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item));
-    console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList.join('');
-};
+    }
+}
 
-renderPage(products);
+let page = new ProductList();
+page.render();
